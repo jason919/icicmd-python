5c6c4963ecf2d5b04b2ac7cc31f9ca41c5c78f98
jhu <jhu@etcc.com>
2023-08-25 16:50:28 -0500

    first

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from loadbalancer import Backendset, Listener, Loadbalancer
 #     "ocid1.loadbalancer.oc1.iad.aaaaaaaajgko2vqbqp5kq6ifhysov3lva2kfel6bhymo2qilnn535dq3kpda",
 #     "new-octa-uat-lb-public",
 # )
-# Loadbalancer.backup_all_loadbalancers()
+Loadbalancer.backup_all_loadbalancers()
 # Backendset.print_all_ip_port()
 # Listener.update_listeners("octa-test-lb.json", "new-octa-test-lb")
-Backendset.update_backend_sets("octa-test-lb.json", "new-octa-test-lb", "SSO")
+# Backendset.update_backend_sets("octa-test-lb.json", "new-octa-test-lb", "SSO")
