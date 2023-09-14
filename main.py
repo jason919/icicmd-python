4c0e6a4443b095bef2b4833eb93dee96ba7ef3ee
Jason Hu <jhu@etcc.com>
2023-09-13 19:49:37 -0500

    first

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from loadbalancer import Backendset, Listener, Loadbalancer
 # Loadbalancer.create_loadbalancer(
 #     "test.json", "lb_test", get_compartment_id("octa-prod")
 # )
-Loadbalancer.create_loadbalancer(
-    "octa-prod-lb.json", "new-octa-prod-lb", get_compartment_id("octa-prod")
-)
+# Loadbalancer.create_loadbalancer(
+#     "octa-prod-lb.json", "new-octa-prod-lb", get_compartment_id("octa-prod")
+# )
 # with open(
 #     "C:/drive-d/projects/etcc/company/CICD/icicmd-python/resources/files/saved/test3.json",
 #     "r",
@@ -21,9 +21,9 @@ Loadbalancer.create_loadbalancer(
 # Loadbalancer.update_display_name(
 #     compartments["octa-uat"],
 #     "ocid1.loadbalancer.oc1.iad.aaaaaaaajgko2vqbqp5kq6ifhysov3lva2kfel6bhymo2qilnn535dq3kpda",
-#     "new-octa-uat-lb-public",
+#     "octa-uat-iva-public",
 # )
-# Loadbalancer.backup_all_loadbalancers()
+Loadbalancer.backup_all_loadbalancers()
 # Backendset.print_all_ip_port()
 # Listener.update_listeners("octa-test-lb.json", "new-octa-test-lb")
 # Backendset.update_backend_sets("octa-test-lb.json", "new-octa-test-lb", "SSO")
